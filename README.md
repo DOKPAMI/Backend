bea71b2d1b42aca44bbbbab046a070cbe7f6f6fc
hyo141 <hyochang141@gmail.com>
2025-02-17 23:23:33 +0900

    Add index.ts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-#DOKPAMI SEASON2 LET'S GET IT!!
+## DOKPAMI SEASON2 LET'S GET IT!!
+
+# with chatgpt
+# https://chatgpt.com/c/67b1f911-a6ec-800d-afb3-09ef46d66381
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+const PORT = 5500;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
