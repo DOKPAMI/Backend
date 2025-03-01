439c7c0af4e6d6f1ac0e8b94fa91ef36bbae58fb
guboc11 <guboc11@gmail.com>
2025-03-01 12:52:35 +0900

    Add codes to ping server every 10 mins to keep the server running

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,23 @@ import { db } from "./db/db.ts";
 
 dotenv.config();
 
+// 10분마다 실행되는 함수
+const fetchResults = () => {
+  const now = new Date().toLocaleString(); // 현재 시각 (로컬 시간대 기준)
+
+  try {
+    fetch("https://backend-60km.onrender.com/balancegame/results").then((res) =>
+      res.json().then((data) => console.log(now, "response:", data))
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+// 10분(600,000ms)마다 실행
+fetchResults();
+setInterval(fetchResults, 600000);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
